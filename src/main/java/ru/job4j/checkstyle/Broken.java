e94eda32e4fd8ec6d398ff656fe162fbb250a516
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-25 23:22:26 +0500

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    public static final String NEW_VALUE = "";
+    public String surname;
+    String name;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d) {
+
+    }
+}
