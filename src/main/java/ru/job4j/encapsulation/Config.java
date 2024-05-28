180dbcc4fa82317ce2a89f47e611ad5dd7df6ba6
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-28 17:21:04 +0500

    0. Что такое инкапсуляция. [#3818] (Задание)

---
+++ src/main/java/ru/job4j/encapsulation/Config.java
@@ -0,0 +1,24 @@
+package ru.job4j.encapsulation;
+
+public class Config {
+    private String name;
+    private int position;
+    private String[] properties;
+
+    public Config(String name) {
+        this.name = name;
+    }
+
+    public void print() {
+        System.out.println(position);
+    }
+
+    public String getProperties(String key) {
+        return search(key);
+    }
+
+    public String search(String key) {
+        return key;
+    }
+
+}
