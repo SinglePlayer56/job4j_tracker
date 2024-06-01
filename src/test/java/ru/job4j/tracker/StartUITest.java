e09173932744285061d489d436711c597b0fb995
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-06-01 12:32:18 +0300

    8. Input и полиморфизм. [#181094] (Исправление)

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -24,9 +24,9 @@ class StartUITest {
         StartUI.createItem(input, tracker);
         Item created1 = tracker.findAll()[0];
         Item created2 = tracker.findAll()[1];
-        Item expected1 = new Item("Fix PC");
-        Item expected2 = new Item("Buy products");
-        assertThat(created1.getName()).isEqualTo(expected1.getName());
-        assertThat(created2.getName()).isEqualTo(expected2.getName());
+        Item expected = new Item("Fix PC");
+        assertThat(created1.getName()).isEqualTo(expected.getName());
+        expected = new Item("Buy products");
+        assertThat(created2.getName()).isEqualTo(expected.getName());
     }
 }
