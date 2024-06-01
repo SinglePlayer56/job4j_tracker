f401319cb7854f9802f778e6789bdba80c35392f
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-06-01 12:56:50 +0300

    9.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
@@ -28,5 +29,29 @@ class StartUITest {
         assertThat(created1.getName()).isEqualTo(expected.getName());
         expected = new Item("Buy products");
         assertThat(created2.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.replaceItem(new MockInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    void whenDeleteItemIsNull() {
+        String[] answers = {"2"};
+        Tracker tracker = new Tracker();
+        Input input = new MockInput(answers);
+        Item item = tracker.findById(2);
+        StartUI.deleteItem(input, tracker);
+        assertThat(item).isNull();
     }
 }
