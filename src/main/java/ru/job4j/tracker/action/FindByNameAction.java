6d3b1dd6937b7c604c036666a7a25cdf5d6744bb
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-06-11 02:37:51 +0500

    9. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/action/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/action/FindByNameAction.java
@@ -4,6 +4,9 @@ import ru.job4j.tracker.*;
 import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.output.Output;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindByNameAction implements UserAction {
     private final Output output;
 
@@ -20,8 +23,8 @@ public class FindByNameAction implements UserAction {
     public boolean execute(Input input, Tracker tracker) {
         output.println("=== Вывод заявок по имени ===");
         String name = input.askStr("Введите имя: ");
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
+        List<Item> items = tracker.findByName(name);
+        if (!items.isEmpty()) {
             for (Item item : items) {
                 output.println(item);
             }
