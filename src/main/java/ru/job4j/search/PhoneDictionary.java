a29bebcc51974c60c6204dd2f60a5882a92c2cb9
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-06-11 00:57:23 +0500

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,31 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список пользователей, которые прошли проверку.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> results = new ArrayList<>();
+        for (Person person : persons) {
+            if (key.contains(person.getName())
+                    || key.contains(person.getSurname())
+                    || key.contains(person.getAddress())
+                    || key.contains(person.getPhone())
+            ) {
+                results.add(person);
+            }
+        }
+        return results;
+    }
+}
