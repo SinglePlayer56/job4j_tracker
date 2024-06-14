21af955b8e64806b9a83cb90523a5027fdfca2e2
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-06-14 11:14:55 +0500

    0. Map, HashMap [#212652 #500622] (Исправление)

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,10 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("test@test.com", "Ivanov Ivan");
         map.put("test2@test.com", "Antonov Anton");
+        map.put("test@test.com", "Semenov Egor");
+        map.put("test45@test.com", "Osipov Anton");
+        map.put("test129@test.com", "Maksimov Maksim");
+        map.put("test11@test.com", "Testov Test");
         for (String key : map.keySet()) {
             System.out.println(key + " : " + map.get(key));
         }
