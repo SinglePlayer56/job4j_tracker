37fb2af00210bce6746a825d26908920fba73faa
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-06-15 14:13:50 +0500

    2. Что такое JavaDoc [#313879]

---
+++ src/main/java/ru/job4j/search/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * В этом пакете расположены классы, созданные при выполнении задания "4. Телефонный справочник на базе ArrayList"
+ * <p>пакет дополнен классом Priority Queue </p>
+ * @since 15.06.24
+ * @author Viktor Korchagin
+ * @version 1.0
+ */
+package ru.job4j.search;
